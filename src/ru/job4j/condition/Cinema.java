199de3647f1ebe428db6553a7bf9b67a8703056b
ArvikVan <arvikvan@gmail.com>
2021-02-19 09:37:43 +0300

    4.3.1. Оператор && [#475645]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -10,8 +10,20 @@ public class Cinema {
         }
     }
 
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
     public static void main(String[] args) {
-        Cinema.acces(21);
-        Cinema.acces(16);
+        /*Cinema.acces(21);
+        Cinema.acces(16);*/
+        Cinema.permission(true, true);
+        Cinema.permission(false, true);
+        Cinema.permission(true, false);
+        Cinema.permission(false, false);
     }
 }
