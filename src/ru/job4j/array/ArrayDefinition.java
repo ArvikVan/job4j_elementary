4ab136ea794c51e4876bc66275872a8aca6e653c
ArvikVan <arvikvan@gmail.com>
2021-02-23 01:29:11 +0300

    6.0.2. Заполнение массива. [#475673]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива surnames: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива prices: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Arviy Khubetsov";
+        names[2] = "Sergey Chernikov";
+        names[3] = "Ivan Youshin";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
