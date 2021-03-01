b2ba0cefa09d4b2874cceb751c62929a0481c0cf
ArvikVan <arvikvan@gmail.com>
2021-03-01 22:52:33 +0300

    1. Экзамен. Базовый синтаксис. [#475736] 3 Объединить два отсортированных массива

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+        int count = 0, countLeft = 0, countRight = 0;
+        while (count < rsl.length) {
+            if (left.length == 0) {
+                rsl = right;
+                break;
+            }
+            if (right.length == 0) {
+                rsl = left;
+               break;
+            }
+            rsl[count] = (left[countLeft] < right[countRight] ? left[countLeft++] : right[countRight++]);
+            if (countLeft == left.length) {
+                System.arraycopy(right, countRight, rsl, ++count, right.length - countRight);
+                break;
+            }
+            if (countRight == right.length) {
+                System.arraycopy(left, countLeft, rsl, ++count, left.length - countLeft);
+                break;
+            }
+            count++;
+        }
+        return rsl;
+    }
+}
